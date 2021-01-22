4c872a2821e12871ff073f67e64af4a4912c867e
Hoang Kim Duc <duchk1@fsoft.com.vn>
2021-01-22 09:11:55 +0700

    .

--- src/java/controller/productProcess.java
+++ src/java/controller/productProcess.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
 import org.springframework.web.servlet.ModelAndView;
 
 /**
@@ -30,9 +31,16 @@ public class productProcess {
     }
     
     @RequestMapping(value = "/add", method=GET)
-    public ModelAndView Add(HttpServletRequest request, HttpServletResponse response){
+    public ModelAndView AddForm(HttpServletRequest request, HttpServletResponse response){
         ModelAndView mv = new ModelAndView("/jsp/product/product_add.jsp");
         mv.addObject("sizes", new ProductDAO().getSizeList());
         return mv;
+    }
+    
+    @RequestMapping(value = "/add", method=POST)
+    public ModelAndView Add(HttpServletRequest request, HttpServletResponse response){
+        ModelAndView mv = new ModelAndView("/jsp/product/product_add.jsp");
+        mv.addObject("sizes", new ProductDAO().getSizeList());
+        return new ModelAndView("/jsp/product/abc.jsp");
     }
 }
